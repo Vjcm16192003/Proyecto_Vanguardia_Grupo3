821509e2b524f885e4b42ab3581f996901c654c3
Vjcm16192003 <vjcm13162003@gmail.com>
2024-03-05 17:39:12 -0600

    logica de registrar hecha

--- vitalia/server/server.js
+++ vitalia/server/server.js
@@ -39,4 +39,55 @@ app.get("/usuario", async (req, res) => {
     }
 });
 
+
+
+app.post("/usuario", (req, res) => {
+    const {
+      fullname,
+      weight,
+      email,
+      date_of_birth,
+      password,
+      height,
+      gender
+    } = req.body;
+  
+
+
+    
+    // Consultar la cantidad actual de usuarios
+    pool.query('SELECT COUNT(*) AS user_count FROM usuario')
+      .then((result) => {
+        const userCount = parseInt(result.rows[0].user_count, 10);
+        const newUserId = userCount + 1;  // Asignar el nuevo ID
+  
+        console.log("Nuevo ID de usuario:", newUserId);
+        console.log("Nombre Completo:", fullname);
+        console.log("Contraseña:", password);
+        console.log("Altura:", height);
+        console.log("Género:", gender);
+        console.log("Correo:", email);
+        console.log("Fecha de Nacimiento:", date_of_birth);
+        console.log("Peso:", weight);
+  
+        const insertSTMT = `INSERT INTO usuario (user_id, fullname, weight, email, date_of_birth, password, height, gender) 
+                            VALUES (${newUserId}, '${fullname}', ${weight}, '${email}', '${date_of_birth}', '${password}', ${height}, '${gender}');`;
+  
+        pool.query(insertSTMT)
+          .then((response) => {
+            console.log("Datos guardados:", response);
+            res.status(200).send("Usuario registrado exitosamente");
+          })
+          .catch((error) => {
+            console.error("Error al guardar los datos:", error);
+            res.status(500).send("Error al registrar el usuario");
+          });
+      })
+      .catch((error) => {
+        console.error("Error al obtener la cantidad de usuarios:", error);
+        res.status(500).send("Error al registrar el usuario");
+      });
+  });
+  
+
 app.listen(5000, () => console.log("server on localhost 5000"))
--- vitalia/src/Modules/Register.jsx
+++ vitalia/src/Modules/Register.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 import { useNavigate } from 'react-router-dom';
 
 function Register() {
-    return (
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        fullName: '',
+        weight: 0,
+        email: '',
+        date_of_birth: '',
+        password: '',
+        height: 0,
+        gender: 'male',
+    });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        let updatedValue = value;
+
+        // Convertir altura de cm a metros
+        if (name === 'height') {
+            updatedValue = (parseFloat(value) / 100).toFixed(2);
+        }
+
+        setFormData({ ...formData, [name]: updatedValue });
+    };
+
+    const handleRegister = () => {
+        // Convertir el formato de la fecha a 'YYYY-MM-DD'
+        const formattedDate = new Date(formData.date_of_birth).toISOString().split('T')[0];
+
+        const nuevoUsuario = {
+            fullname: formData.fullName,
+            weight: parseFloat(formData.weight).toFixed(2),  // Asegurarse de tener dos decimales
+            email: formData.email,
+            date_of_birth: formattedDate,
+            password: formData.password,
+            height: parseFloat(formData.height).toFixed(2),  // Asegurarse de tener dos decimales
+            gender: formData.gender,
+        };
+
+        fetch('http://localhost:5000/usuario', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(nuevoUsuario),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Usuario registrado con éxito:', data);
+                // Realizar cualquier otra lógica necesaria después de agregar el usuario
+            })
+            .catch(error => {
+                console.error('Error al registrar el usuario:', error);
+            });
+    };
+
+    return (
         <div className="scrollable-page">
             <div className="container1">
                 <div className="registration-box">
@@ -12,49 +65,87 @@ function Register() {
 
                     <div className="form-group">
                         <label htmlFor="fullName">Full Name:</label>
-                        <input type="text" id="fullName" placeholder="Enter your full name" />
+                        <input
+                            type="text"
+                            id="fullName"
+                            name="fullName"
+                            placeholder="Enter your full name"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="weight">Weight:</label>
-                        <input type="number" id="weight" placeholder="Enter your weight (in kg)" />
+                        <input
+                            type="number"
+                            id="weight"
+                            name="weight"
+                            placeholder="Enter your weight (in kg)"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" placeholder="Enter your email" />
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="dob">Date of Birth:</label>
-                        <input type="date" id="dob" />
+                        <input
+                            type="text"  // Cambiado a text
+                            id="dob"
+                            name="date_of_birth"
+                            placeholder="YYYY-MM-DD"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" placeholder="Enter your password" />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="height">Height:</label>
-                        <input type="number" id="height" placeholder="Enter your height (in cm)" />
+                        <input
+                            type="number"
+                            id="height"
+                            name="height"
+                            placeholder="Enter your height (in cm)"
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="gender">Gender:</label>
-                        <select id="gender">
+                        <select
+                            id="gender"
+                            name="gender"
+                            onChange={handleChange}
+                            value={formData.gender}
+                        >
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
                     </div>
 
-
-                    <button type="submit">Sign Up</button>
+                    <button type="submit" onClick={handleRegister}>Sign Up</button>
                 </div>
             </div>
         </div>
-
-
     );
 }
 
