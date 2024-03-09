59647c5d73bfc6501fe970f333390094e8837965
MarcelaRivera2000 <mvrivera@unitec.edu>
2024-03-09 15:07:22 -0600

    AI funcional

---
+++ vitalia/src/Modules/AIpage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './AIpage.css';
+import { useNavigate } from 'react-router-dom';
+
+function AIpage() {
+   
+    return (
+        <>
+        </>
+    );
+}
+
+export default AIpage;
